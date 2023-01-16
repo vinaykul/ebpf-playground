d693815c675a994244334bb63297ff1e2f84d0eb
vinay kulkarni <vskibum@gmail.com>
2023-01-16 21:51:51 +0000

    Add proactive eBPF stats program load yaml for amd64

--- ebpf-pod-network-stats/ebpf-netstats-docker-img/attach_ebpf_netstats_prog.sh
+++ ebpf-pod-network-stats/ebpf-netstats-docker-img/attach_ebpf_netstats_prog.sh
@@ -6,6 +6,10 @@
 # Set this environment variable to provide eBPF section name of stats program
 #export eBPF_SECTION_NAME=""
 
-if [ $1 == 0 ]; then
+#TODO: Investigate why 'retval' from bpftrace is garbage when using upstream bits vs. 'GODEBUG=1 make all' bits
+#      Calling attach_ebpf_netstats_prog.py still works since it aims to attach eBPF program to all containerd
+#      generated network namespaces ('cni-' prefix). Specifying only certain CNI network interfaces (via pod
+#      annotations or some other means) is a possible future work item if that is desired.
+#if [ $1 == 0 ]; then
     python3 /attach_ebpf_netstats_prog.py
-fi
+#fi
